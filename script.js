8e788ddab69b493c1e5fdebb51f55f48acca7048
rafaelokra <122841125+rafaelokra@users.noreply.github.com>
2023-06-01 23:32:26 -0300

    atividade finalizada

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 const home = document.querySelector(".home");
 const luck = document.querySelector(".luck");
 const homeImage = document.querySelector("#btnReset");
+const btnBack = document.querySelector("#btnBack");
 
 function openLuck() {
   home.style.display = "none";
   luck.style.display= "block";
-  
  
  
 }
 
 homeImage.addEventListener("click", openLuck);
 
-const btnBack = document.querySelector("#btnBack");
+
 
 btnBack.addEventListener("click", function() {
   luck.style.display = "none";
   home.style.display = "block";
+ 
   
 });
